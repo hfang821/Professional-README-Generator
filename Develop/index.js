5b3001cc18adfc150ef184f403c5c5a45369f254
hfang821 <95199209+hfang821@users.noreply.github.com>
2022-04-16 15:35:39 -0400

    adding the questions object

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,146 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the project title?',
+        validate: nameInput => {
+            if(nameInput){
+                return true;
+            } else {
+                console.log('Please enter a title.');
+                return false;
+            }
+        }
+    },
+    
+    {
+        type: 'input',
+        name: 'description',
+        message: "What's the description of this project?",
+        validate: descriptionInput => {
+            if(descriptionInput) {
+                return true;
+            } else {
+                console.log('Please enter a description.');
+                return false;
+            }
+        }
+    },
+    
+    {
+        type: 'input',
+        name: 'installation',
+        message: "What are the installation instructions of this project?",
+        validate: installInput => {
+            if(installInput) {
+                return true;
+            } else {
+                console.log('Please enter installation instructions.');
+                return false;
+            }
+        }
+    },
+
+    {
+        type: 'input',
+        name: 'usage',
+        message: "What's the usage information of this project?",
+        validate: usageInput => {
+            if(usageInput) {
+                return true;
+            } else {
+                console.log('Please enter the usage information.');
+                return false;
+            }
+        }
+    },
+
+    {
+        type: 'input',
+        name: 'guidelines',
+        message: "What are the contribution guidelines of this project?",
+        validate: guidelineInput => {
+            if(guidelineInput) {
+                return true;
+            } else {
+                console.log('Please enter contribution guidelines.');
+                return false;
+            }
+        }
+    },
+
+    {
+        type: 'input',
+        name: 'test',
+        message: "What are the test instructions of this project?",
+        validate: testInput => {
+            if(testInput) {
+                return true;
+            } else {
+                console.log('Please enter a description.');
+                return false;
+            }
+        }
+    },
+
+    {
+        type: 'confirm',
+        name: 'license',
+        message: 'Would you like to include a license for this project?',
+        default: true
+    },
+
+    {
+        type: 'list',
+        name: 'licenseOptions',
+        message: 'Choose one of the following license options.',
+        choices: ['Apache 2.0','Boost','BSD', 'Eclipse', 'MIT', 'Mozilla Public License 2.0']
+    },
+
+    {
+        type: 'input',
+        name: 'username',
+        message: 'Please enter your github username.',
+        validate: usernameInput => {
+            if(usernameInput){
+                return true;
+            } else {
+                console.log('Please enter your github username!');
+                return false;
+            }
+        }
+    },
+
+    {
+        type: 'input',
+        name: 'contact',
+        message: 'Please enter how you want to be reached if users have any questions.',
+        validate: contactInput => {
+            if(contactInput) {
+                return true;
+            } else {
+                console.log('Please enter your contact instructions!')
+                return false;
+            }
+        }
+    }
+];
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    //inquirer.prompt
+
+}
 
 // Function call to initialize app
 init();
